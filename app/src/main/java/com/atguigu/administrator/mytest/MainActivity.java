fca14c704001c5932d4cefbab0447680ad4fe059
duckclicker <547435220@qq.com>
2017-01-16 15:13:57 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/administrator/mytest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.atguigu.administrator.mytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioGroup rg_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rg_main = (RadioGroup) findViewById(R.id.rg_main);
+
+        rg_main.check(R.id.rb_local_video);
+    }
+}
